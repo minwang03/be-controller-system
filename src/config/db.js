140222808fdb17b68dcae6df4d0d5d69bab16234
minwang03 <93901911+minwang03@users.noreply.github.com>
2025-04-18 10:46:49 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  user: process.env.DB_USER || 'root1',
+  password: process.env.DB_PASSWORD || 'root1',
   database: process.env.DB_NAME || 'QuangDB',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
